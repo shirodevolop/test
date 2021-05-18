6b52d2c4fc468cdf54ee4d19485618f613d056c3
shirodevolop <emilioelva11@gmail.com>
2021-05-18 08:57:33 +0700

    test lu

---
+++ example/test_again_example.dart
@@ -0,0 +1,6 @@
+import 'package:test_again/test_again.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/test_again.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library test_again;
+
+export 'src/test_again_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
